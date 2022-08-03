056de842197317fb81fbf0ae0112ff9a32c5b1b9
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-03 14:59:59 -0300

    (03/08) Using e Dispose

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,42 +1,26 @@
 ﻿using System;
-using System.Data;
 
 namespace Payments
 {
     class Program
     {
         static void Main(string[] args)
+        {   
+            using(var pagamento = new Pagamento()){
+                Console.WriteLine("Processando o pagamento");
+            }
+        }
+    }
+
+
+    public class Pagamento : IDisposable{
+        public Pagamento(){
+            Console.WriteLine("Iniciando o construtor");
+        }
+
+        public void Dispose()
         {
-            var pagamento = new Pagamento(DateTime.Now);
-            pagamento.Pagar("123456");
-            
-            Console.WriteLine("Hello!");
+            Console.WriteLine("encerramento pagamento");
         }
-    }
-
-
-    public class Pagamento{
-
-        public DateTime DataPagamento { get; set; }
-        public Pagamento(DateTime vencimento){
-            Console.WriteLine("Iniciando o pagamento");
-            DataPagamento = DateTime.Now;
-        }
-
-        public virtual void Pagar(string numero) {
-            Console.WriteLine("Pagar");
-        }
-
-    }
-
-    public class PagamentoCartao : Pagamento{
-        public override void Pagar(string numero) {
-            base.Pagar(numero);
-            Console.WriteLine("Pagar com cartão");
-        }
-    }
-
-    public class Address{
-        string ZipCode;
     }
 }
