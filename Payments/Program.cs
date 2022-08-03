d467b9aaf5bce99b09392d7b2403ff134beaad63
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-03 15:16:37 -0300

    (03/08) classes seladas

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -6,15 +6,12 @@ namespace Payments
     {
         static void Main(string[] args)
         {   
-            Pagamento.Vencimento = DateTime.Now;
-            Console.WriteLine(Pagamento.Vencimento);
+            Console.WriteLine("hello!");
         }
     }
 
 
     public static class Pagamento{
         public static DateTime Vencimento { get; set; }
-
-        
     }
 }
