477abf87a90552a99a1606abe966c3cf576e082b
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-03 16:59:36 -0300

    (03/08) delegates

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,35 +1,20 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Payments
 {
     class Program
     {
-        static void Main(string[] args)
-        {   
-            Console.WriteLine("hello!");
-            // pessoaA == pessoaB => false
-            var pessoaA = new Pessoa(1, "Rafael Bortolozo");
-            var pessoaB = new Pessoa(1, "Rafael Bortolozo");
-
-            Console.WriteLine(pessoaA.Equals(pessoaB));
-
+        static void RealizarPagamento(double valor){
+            Console.WriteLine($"Pago o valor de {valor} reais");
+        }
+        static void Main(string[] args){   
+            var metodo = new Pagamento.Pagar(RealizarPagamento);
+            metodo(25);
         }
     }
 
-    public class Pessoa : IEquatable<Pessoa>{
-        public Pessoa(int id, string nome){
-            Id = id;
-            Nome = nome;
-        }
-
-        public int Id { get; set; }
-        public string Nome { get; set;}
-
-        public bool Equals(Pessoa other)
-        {
-            return Id == other.Id;
-        }
-    }  
+    public class Pagamento {
+        public delegate void Pagar(double valor);
+    }
 
 }
