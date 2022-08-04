866f0e383ec3ea8b9d6143210be91a74caae3fe3
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-04 18:00:41 -0300

    (04/08) Projeto do Balta

---
+++ Balta/ContentContext/Module.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Balta.ContentContext{
+    public class Module : Base{
+        public Module()
+        {
+            Lectures = new List<Lecture>();
+        }
+        public int Order { get; set; }
+        public int Title { get; set; }
+        public IList<Lecture> Lectures { get; set; }
+    }
+}
