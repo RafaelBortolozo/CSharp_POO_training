5ccaf015e396e1a08aadc6b4d2779275dbdce2f6
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-08-04 15:59:45 -0300

    (04/08) Lists

--- Payments/Program.cs
+++ Payments/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Payments
 {
@@ -6,28 +9,46 @@ namespace Payments
     {
         static void Main(string[] args){   
             Console.WriteLine("Hello");
-            var context = new DataContext<IPerson, Payment, Subscription>();
-            context.Save(new Payment());
-            context.Save(new Person());
-            context.Save(new Subscription());
+            // PARTE 1
+            // IEnumerable<Payment> payments = new List<Payment>();
+            // IList<Payment> payments2 = new List<Payment>(); // mais métodos
+
+
+            // PARTE 2
+            // var payments = new List<Payment>();
+            // payments.Add(new Payment(1));
+            // payments.Add(new Payment(10));
+            // payments.Add(new Payment(9));
+            // payments.Add(new Payment(5));
+            // payments.Add(new Payment(4));
+            // payments.Add(new Payment(7));
+
+            // foreach(var payment in payments){
+            //     Console.WriteLine(payment.Id);
+            // }
+
+            // var paidPayments = new List<Payment>();
+            // paidPayments.AddRange(payments); // concatena
+
+            // var payment2 = payments.Where(x => x.Id > 5);
+            // //var payment = payments.Firts(x => x.Id > 5); // Retorna um elemento
+
+            // Tem um monte de metodos, melhor pesquisar na documentação.
+
+
+            // PARTE 3
+            IEnumerable<Payment> payments = new List<Payment>();
+            payments.ToList();
+            payments.AsEnumerable();
+            payments.ToArray();
         }
     }
 
-    public class DataContext<T, U, V> 
-        where T : IPerson
-        where U : Payment
-        where V : Subscription 
-    {
-        public void Save(T entity){ }
-        public void Save(U entity){ }
-        public void Save(V entity){ }
+    public class Payment{
+        public int Id { get; set; }
+        public Payment(int id){
+            Id = id;
+        }
+
     }
-
-    public interface IPerson { }
-
-    public class Person : IPerson{ }
-
-    public class Payment{ }
- 
-    public class Subscription{ }
 }
